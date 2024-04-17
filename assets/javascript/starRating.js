dddb1ea9503f7b8b4cdeb3573e4b7e4aad4b2002
Quentin <118888713+QuentinB0@users.noreply.github.com>
2024-04-17 16:05:04 +0200

    feat(show): change frontend show

--- assets/javascript/starRating.js
+++ assets/javascript/starRating.js
@@ -1,11 +1,14 @@
 const notes = document.querySelectorAll('.star-ratings')
 
 
-for(let note of notes) {
-     let stars = ''
+for (let note of notes) {
+    let stars = ''
     const valeurNote = parseFloat(note.dataset.note);
-    for(i=0;i<valeurNote;i++) {
-        stars +='⭐'
+    for (i = 0; i < valeurNote; i++) {
+        stars += '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64"><path fill="#ffce31" d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2z" /></svg>'
     }
-    note.textContent = stars
+    for (let j = valeurNote; j < 5; j++) {
+        stars += '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 64 64"><path fill="white" d="m61.13 23.718l-22.65-.105L31.583.692l-6.898 22.921l-22.651.105L20.423 38.35l-9.297 24.96l20.457-15.86L52.05 63.31l-9.308-24.96z"/><path fill="white" d="M53.09 26.904L38.48 24.22l-6.897-12.27l-6.898 12.27l-14.08 2.84l9.814 11.891l-2.572 15.85l13.732-6.751l14.11 6.903l-2.955-16z"/></svg>'
+    }
+    note.innerHTML = stars
 }
