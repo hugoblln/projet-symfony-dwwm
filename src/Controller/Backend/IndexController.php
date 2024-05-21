8bbfbc9dad37918efea41bcda441ee96a055ab3b
hugo bellin <hugobellin@yahoo.com>
2024-05-21 08:08:43 +0200

    feat(admin) : make route for admin index page

--- src/Controller/Backend/IndexController.php
+++ src/Controller/Backend/IndexController.php
@@ -3,11 +3,12 @@
 namespace App\Controller\Backend;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
-    #[Route('admin/index', 'admin.index', ['GET'] )]
+    #[Route('admin/index', 'admin.index', methods: ['GET'] )]
     public function index() : Response 
     {
         return $this->render('backend/index/index.html.twig');
