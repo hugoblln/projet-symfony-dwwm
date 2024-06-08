e8078b21fac3bf2179406580a9486eec52eba0c3
hugo bellin <hugobellin@yahoo.com>
2024-06-08 17:31:45 +0200

    feat(creneaux): make requete for each complexe creneaux

--- src/Repository/CreneauxRepository.php
+++ src/Repository/CreneauxRepository.php
@@ -3,8 +3,8 @@
 namespace App\Repository;
 
 use App\Entity\Creneaux;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 /**
  * @extends ServiceEntityRepository<Creneaux>
@@ -19,6 +19,27 @@ class CreneauxRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Creneaux::class);
+    }
+
+    public function findCreneauxComplexe(int $complexeId ) : array 
+    {
+     
+    $conn = $this->getEntityManager()->getConnection();
+
+    $sql = '
+    SELECT CONCAT(c.debut,\' \', c.fin) as creneau
+    FROM creneaux c
+    CROSS JOIN complexes co
+    WHERE c.debut BETWEEN co.heure_ouverture AND co.heure_fermeture - 1
+    AND co.id = :complexeId;
+    ';
+
+    $resultSet = $conn->executeQuery($sql, ['complexeId' => $complexeId]);
+
+    $result = $resultSet->fetchAll();
+
+    return $result ?? [];
+
     }
 
     //    /**
