d338632fd8c43f73987f27c94b19af361560f1fb
Quentin <118888713+QuentinB0@users.noreply.github.com>
2024-08-26 10:46:14 +0200

    feat(password): add reset password

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true]
 ];
---
+++ src/Entity/ResetPasswordRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ResetPasswordRequestRepository;
+use Doctrine\ORM\Mapping as ORM;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestInterface;
+use SymfonyCasts\Bundle\ResetPassword\Model\ResetPasswordRequestTrait;
+use App\Entity\Users; // Assurez-vous que cette classe est correctement importée
+
+#[ORM\Entity(repositoryClass: ResetPasswordRequestRepository::class)]
+/**
+ * @IgnoreAnnotation("ORM\Column")
+ */
+class ResetPasswordRequest implements ResetPasswordRequestInterface
+{
+    use ResetPasswordRequestTrait;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: Users::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Users $user = null;
+
+    public function __construct(Users $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken)
+    {
+        $this->user = $user;
+        $this->initialize($expiresAt, $selector, $hashedToken);
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): Users
+    {
+        return $this->user;
+    }
+}
